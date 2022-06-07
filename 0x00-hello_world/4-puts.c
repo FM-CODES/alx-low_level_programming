fd5363488038731e9a2b2ba4bb79b727cfdac814
FM-CODES <oluwafemiolowe5@gmail.com>
2022-06-07 05:41:51 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,5 +6,7 @@
  *   *   *Return: Always 0 (Success)
  *    **/
 int main(void)
-{	puts("\"programming is like building a multilingual puzzle");	return(0);											
+{
+          	puts("\"programming is like building a multilingual puzzle");
+	        	return(0);				
 }		
